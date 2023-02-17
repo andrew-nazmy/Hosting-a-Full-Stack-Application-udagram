e6928cb01653bffc4c6a418f80d8f125802c9f2f
Andrew George Nazmy | Ejada Systems Ltd <anazmy@ejadacairo.local>
2023-02-17 16:09:14 +0200

    prod env fix

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "hudagram-api-dev.eba-ndcpqng5.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-ndcpqng5.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
